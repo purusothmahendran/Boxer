3dd484d15e0f8f459bad1dc4dcd797c675f3ce1e
Purusoth Mahendran <purusoth1091@gmail.com>
2013-05-02 10:10:51 +0530

    Update boxer.py

--- boxer.py
+++ boxer.py
@@ -1,86 +1,89 @@
-from twython import Twython
-import string, json, pprint
-import urllib
-from datetime import timedelta
-from datetime import date
-from time import *
-import string, os, sys, subprocess, time
+import csv
+import nltk
+import re
+from nltk.corpus import stopwords
+tweets=[]
+with open('C:/users/purusoth/desktop/trainingset2000.csv','rb') as f:
+    reader=csv.reader(f)
+    for row in reader:
+        tweets.append(row)
+#print tweets
+tweetssenti=[]
+for (a,b,c,d,e,f) in tweets:
+    words_filtered=[e.lower() for e in f.split() if len(e)>=3]
+    for i in words_filtered:
+        if i in stopwords.words('english'):
+            #print i
+            words_filtered.remove(i)
+    tweetssenti.append((words_filtered,a))
+#print tweetssenti
+print 'tweent senti done'
 
+def get_words_in_tweets(tweetssenti):
+    all_words = []
+    for (words, sentiment) in tweetssenti:
+      all_words.extend(words)
+    return all_words
+def get_word_features(wordlist):
+    wordlist = nltk.FreqDist(wordlist)
+    #print wordlist
+    word_features = wordlist.keys()
+    return word_features
+word_features = get_word_features(get_words_in_tweets(tweetssenti))
+print 'word_features done'
+#print word_features
 
-# probably wasteful imports, but in this case, who cares?
-import MySQLdb
-
-conn = MySQLdb.connect('localhost','root','','tweetharvester')
-cur = conn.cursor()
-# connect to our database and create a cursor to do some work
-
-harvest_list = ['vishwaroopam', 'viswaroopam', 'Kamal Hasan', 'Kamal Hassan']
-# my aforementioned harvest list, use as many as you want,
-# they will all be separated in the database by keyword
-
-
-cur.execute("select max(isnull(batchid)) from tweetlog")
-batch_id = cur.fetchall()
-#batch_id = batch_id_cur[0][0]+1
-
-#grabbing the last "batch id", if it exists so we
-# can make log entries that make SOME sense
-
-
-
-for tweet_keyword in harvest_list: # for each keyword, do some shit
-
-        cur.execute("""delete from tweetbanktemp where tweet_keyword = '"""+str(tweet_keyword)+"""'""")
-        conn.commit()
-        # whack the temp table in case we didn't exit cleanly
-
-        twitter = Twython()
-        search_results = twitter.search(q=tweet_keyword, rpp="100")
-        # our search for the current keyword
-
-        #pp = pprint.PrettyPrinter(indent=3)
-        # uncomment for debugging and displaying pretty output
-
-        for tweet in search_results["results"]:
-                # some me the tweet, jerry!
-                print "        Tweet from @%s Date: %s" % (tweet['from_user'].encode('utf-8'),tweet['created_at'])
-                print "        ",tweet['text'].encode('utf-8'),"\n"
-
-                try:
-                        # lets try to to put each tweet in our temp table for now
-                        cur.execute("""insert into TweetBankTemp (tweet_id, tweet_datetime, tweet_keyword, tweet, tweeter, lang)
-                                        values ('"""+str(tweet['id_str'].encode('utf-8').replace("'","''").replace(';',''))+"""',
-                                                cast(substring('"""+str(tweet['created_at'].encode('utf-8'))+"""',5,21) as datetime),
-                                                '"""+str(tweet_keyword)+"""',
-                                                '"""+str(tweet['text'].encode('utf-8').replace("'","''").replace(';',''))+"""',
-                                                '"""+str(tweet['from_user'].encode('utf-8').replace("'","''").replace(';',''))+"""',
-                                                '"""+str(tweet['iso_language_code'].encode('utf-8').replace("'","''").replace(';',''))+"""'
-                                        ) """)
-                except:
-                        print "############### Unexpected error:", sys.exc_info()[0], "##################################"
-                        # just in case something heinous happens we don't lose
-                        # ALL the tweets in this run, just the error one
-
-
-        cur.execute("""insert into tweetbank (tweet_id, tweet_datetime, tweet_keyword, tweet, tweeter, lang)
-        select * from tweetbanktemp where tweet_id NOT in
-        (select distinct tweet_id from tweetbank)""")
-        # take all the tweets that we DIDNT already have
-        # and put them in the REAL tweet table
-
-        cur.execute("""delete from tweetbanktemp where tweet_keyword = '"""+str(tweet_keyword)+"""'""")
-        # take all THESE out of the temp table to not
-        # interfere with the next keyword
-
-       # cur.execute("""insert into tweetlog (BatchId, keyword, RunDate, HarvestedThisRun, TotalHarvested) values
-        #(
-        #'"""+str(batch_id)+"""',
-       # '"""+str(tweet_keyword)+"""',
-       # getdate(),
+def extract_features(document):
+    document_words = set(document)
+    features = {}
+    for word in word_features:
+        features['contains(%s)' % word] = (word in document_words)
         
-#((select count(*) from tweetbank where tweet_keyword = '"""+str(tweet_keyword)+"""')-(select isnull(TotalHarvested) from tweetlog where keyword = '"""+str(tweet_keyword)+"""' order by RunDate desc limit 1)), (select count(*) from tweetbank where tweet_keyword = '"""+str(tweet_keyword)+"""')
-      #  )""")
-        # add a record to the log table saying what we did!
+    return features
+print 'obtaining training set'   
+training_set = nltk.classify.util.apply_features(extract_features, tweetssenti)
+#print training_set
+print 'training the classfier .....'
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print ' Trained!'
+print classifier.show_most_informative_features(200)
+def processTweet(tweet):
+    # process the tweets
 
-        conn.commit()
-        # hot soup!
+    #Convert to lower case
+    tweet = tweet.lower()
+    #Convert www.* or https?://* to URL
+    tweet = re.sub('((www\.[\s]+)|(https?://[^\s]+))','',tweet)
+    #Convert @username to AT_USER
+    tweet = re.sub('@[^\s]+','',tweet)
+    #Remove additional white spaces
+    tweet = re.sub('[\s]+', ' ', tweet)
+    #Replace #word with word
+    tweet = re.sub(r'#([^\s]+)', '', tweet)
+    #trim
+    tweet = tweet.strip('\'"')
+    return tweet
+#end
+
+test_tweet=[]
+with open('C:/Users/Purusoth/Desktop/movies/gijoe.csv','rb') as s:
+    reader=csv.reader(s)
+    for row in reader:
+       test_tweet.append(row)
+    print test_tweet
+testing=[]
+for (d,e,f,g,h) in test_tweet:
+    #print d
+    #print 'preprocessing'
+    filtered=processTweet(e)
+    tweet_test=filtered.split()
+    for i in tweet_test:
+        if i in stopwords.words('english'):
+            print i
+            tweet_test.remove(i)
+    #print filtered
+    testing.append(tweet_test)
+for tw in testing:
+    print tw
+    print classifier.classify(extract_features(tw))
+
